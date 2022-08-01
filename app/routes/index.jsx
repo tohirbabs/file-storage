6c0f1da556c5973952e1974bd71b27c8c01b1744
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-08-01 20:56:40 +0100

    New changes

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -50,9 +50,13 @@ export default function Dash() {
       <div className="h-full flex bg-nova-grey w-full flex-col ml-[280px] xl:ml-[300px]">
         {/* Header */}
         <div className="grid grid-cols-4 lg:h-[70px] content-center gap-3 w-[calc(100vw-280px)] xl:w-[calc(100vw-300px)] header_custom fixed top-0 left-[280px] xl:left-[300px] bg-nova-grey z-50 py-2">
-          <div className="col-span-1 flex justify-center flex-col content-center">
-            <p>welcome to your dashboard</p>
-            <h3>Ayomide Matins</h3>
+          <div className="col-span-1 flex lg:pl-6 xl:pl-11 text-white justify-center flex-col content-center">
+            <p className="text-base lg:text-sm xl:text-base">
+              Welcome to your dashboard
+            </p>
+            <h3 className="text-2xl lg:text-xl xl:text-2xl font-semibold">
+              Ayomide Martins
+            </h3>
           </div>
 
           <div className="col-span-2 flex justify-center content-center">
@@ -88,7 +92,82 @@ export default function Dash() {
           </div>
 
           <div className="col-span-1 flex flex-row content-center justify-center">
-            <span className="text-center">Abdur-Rahman Fashola</span>
+            <button
+              id="dropdownAvatarNameButton"
+              data-dropdown-toggle="dropdownAvatarName"
+              class="flex items-center text-sm font-medium text-gray-900 rounded-full hover:text-blue-600 dark:hover:text-blue-500 md:mr-0 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-white"
+              type="button"
+            >
+              <span class="sr-only">Open user menu</span>
+              <img
+                class="mr-2 w-8 h-8 rounded-full"
+                src="https://i.pravatar.cc/200"
+                alt="user photo"
+              />
+              Bonnie Green
+              <svg
+                class="w-4 h-4 mx-1.5"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+
+
+            {/* Dropdown for header user menu */}
+            <div
+              id="dropdownAvatarName"
+              class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+            >
+              <div class="py-3 px-4 text-sm text-gray-900 dark:text-white">
+                <div class="font-medium ">Pro User</div>
+                <div class="truncate">name@flowbite.com</div>
+              </div>
+              <ul
+                class="py-1 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownInformdropdownAvatarNameButtonationButton"
+              >
+                <li>
+                  <a
+                    href="#"
+                    class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Dashboard
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Earnings
+                  </a>
+                </li>
+              </ul>
+              <div class="py-1">
+                <a
+                  href="#"
+                  class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Sign out
+                </a>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -102,13 +181,22 @@ export default function Dash() {
           <section className="grid grid-cols-1 lg:grid-cols-5 gap-3 lg:gap-5 content-center justify-between px-4 mt-8 w-full">
             <ClientOnly>
               {() => (
-                <div className="text-white rounded-lg px-9 grid col-span-3 pt-5 pb-2 h-full bg-nova-black">
+                <div className="text-white rounded-lg px-9 grid col-span-5 pt-5 pb-2 h-full bg-nova-black">
                   <div className="text-center w-full text-2xl font-bold">
                     Overview
                   </div>
                 </div>
               )}
             </ClientOnly>
+          </section>
+
+          <section className="grid grid-cols-1 lg:grid-cols-5 gap-3 lg:gap-5 content-center justify-between px-4 mt-8 w-full">
+            <div className="text-white rounded-lg px-9 grid col-span-3 pt-5 pb-2 h-full bg-nova-black">
+              <div className="text-center w-full text-2xl font-bold">
+                Last Flight Data
+              </div>
+            </div>
+
             <div className="text-white rounded-lg px-9 pt-5 h-full grid col-span-2 bg-nova-black">
               <div className="text-center w-full text-2xl font-bold">
                 Next Flight Details
@@ -131,20 +219,6 @@ export default function Dash() {
                   <span className="font-bold">Nova One</span>
                 </li>
               </ul>
-            </div>
-          </section>
-
-          <section className="grid grid-cols-1 lg:grid-cols-5 gap-3 lg:gap-5 content-center justify-between px-4 mt-8 w-full">
-            <div className="text-white rounded-lg px-9 grid col-span-3 pt-5 pb-2 h-full bg-nova-black">
-              <div className="text-center w-full text-2xl font-bold">
-                Last Flight Data
-              </div>
-            </div>
-
-            <div className="text-white rounded-lg px-9 pt-5 h-full grid col-span-2 bg-nova-black">
-              <div className="text-center w-full text-2xl font-bold">
-                Available Services
-              </div>
             </div>
           </section>
         </div>
